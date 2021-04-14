5d0a699c2df6690058918a24922c30a8d0dde7b9
Ivan <korotkov.ivan.s@gmail.com>
2021-04-14 22:03:51 +0500

    feat: remove extra logic and add timeout to droplet fetch

--- src/connect.ts
+++ src/connect.ts
@@ -1,9 +1,5 @@
 import * as vscode from 'vscode'
 
-export function getConnectionString (host: string, username: string) {
-  return `ssh ${username}@${host} -A`
-}
-
 export async function connectToHost (host: string, username: string, path:string) {
   const uri = vscode.Uri.parse(`vscode-remote://ssh-remote+${username}@${host}${path}`)
   await vscode.commands.executeCommand('vscode.openFolder', uri, false)
--- src/extension.ts
+++ src/extension.ts
@@ -3,46 +3,40 @@ import { createApiClient } from 'dots-wrapper'
 import { IDroplet } from 'dots-wrapper/dist/modules/droplet'
 import {} from 'tslib'
 import { createDroplet, getDropletIP, hasPublicIP, selectDroplet } from './digitalocean/droplets'
-import { readConfig } from './config'
-import { connectToHost, getConnectionString } from './connect'
-
-async function copyOrConnect (droplet: IDroplet, username: string, path: string) {
-  const host = getDropletIP(droplet)
-
-  const copyOption = 'Copy SSH command'
-  const connectOption = 'Connect'
-
-  const selection = await vscode.window.showInformationMessage('Remote VM is ready', copyOption, connectOption)
-
-  if (selection === copyOption) {
-    await vscode.env.clipboard.writeText(getConnectionString(host, username))
-  } else if (selection === connectOption) {
-    await connectToHost(host, username, path)
-  } else {
-    throw Error('Nothing was selected')
-  }
-}
+import { connectToHost } from './connect'
 
 export async function activate (context: vscode.ExtensionContext) {
-  const config = readConfig()
   const username = 'root'
   const path = '/home'
 
+  const config = vscode.workspace.getConfiguration('remote-vm')
+  const token = config.get<string>('do-token')
+
+  if (!token) {
+    throw Error('No token was specified in config. Specify token and retry')
+  }
+
   const client = createApiClient({
-    token: config.token
+    token: token
   })
 
   context.subscriptions.push(vscode.commands.registerCommand('remote-vm.createVM', async () => {
     try {
       const createdDroplet = await createDroplet(client)
 
-      let fetchedDroplet: IDroplet | undefined
-      while (fetchedDroplet === undefined || hasPublicIP(fetchedDroplet)) {
-        const dropletResponse = await client.droplet.getDroplet({ droplet_id: createdDroplet.id })
-        fetchedDroplet = dropletResponse.data.droplet
+      const tryToConnect = async () => {
+        let fetchedDroplet: IDroplet | undefined
+        while (fetchedDroplet === undefined || hasPublicIP(fetchedDroplet)) {
+          const dropletResponse = await client.droplet.getDroplet({ droplet_id: createdDroplet.id })
+          fetchedDroplet = dropletResponse.data.droplet
+        }
+
+        const host = getDropletIP(fetchedDroplet)
+
+        await connectToHost(host, username, path)
       }
 
-      await copyOrConnect(fetchedDroplet, username, path)
+      setTimeout(tryToConnect, 1000)
     } catch (error) {
       vscode.window.showErrorMessage(`Error occurred: ${error}`)
     }
@@ -53,8 +47,9 @@ export async function activate (context: vscode.ExtensionContext) {
       const dropletsResponse = await client.droplet.listDroplets({})
       const droplets = dropletsResponse.data.droplets
       const droplet = await selectDroplet(droplets)
+      const host = getDropletIP(droplet)
 
-      await copyOrConnect(droplet, username, path)
+      await connectToHost(host, username, path)
     } catch (error) {
       vscode.window.showErrorMessage(`Error occurred: ${error}`)
     }
