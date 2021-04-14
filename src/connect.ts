00f91ed8649bf02044f84e4667d7ef858906cc88
Ivan <korotkov.ivan.s@gmail.com>
2021-04-14 21:49:38 +0500

    feat: destroy droplet via command

--- src/connect.ts
+++ src/connect.ts
@@ -1,10 +1,10 @@
 import * as vscode from 'vscode'
 
-export function connectionString (host: string, username: string) {
+export function getConnectionString (host: string, username: string) {
   return `ssh ${username}@${host} -A`
 }
 
-export async function connect (host: string, username: string, path:string) {
+export async function connectToHost (host: string, username: string, path:string) {
   const uri = vscode.Uri.parse(`vscode-remote://ssh-remote+${username}@${host}${path}`)
   await vscode.commands.executeCommand('vscode.openFolder', uri, false)
 }
--- src/extension.ts
+++ src/extension.ts
@@ -4,60 +4,73 @@ import { IDroplet } from 'dots-wrapper/dist/modules/droplet'
 import {} from 'tslib'
 import { createDroplet, getDropletIP, hasPublicIP, selectDroplet } from './digitalocean/droplets'
 import { readConfig } from './config'
-import { connect, connectionString } from './connect'
+import { connectToHost, getConnectionString } from './connect'
+
+async function copyOrConnect (droplet: IDroplet, username: string, path: string) {
+  const host = getDropletIP(droplet)
+
+  const copyOption = 'Copy SSH command'
+  const connectOption = 'Connect'
+
+  const selection = await vscode.window.showInformationMessage('Remote VM is ready', copyOption, connectOption)
+
+  if (selection === copyOption) {
+    await vscode.env.clipboard.writeText(getConnectionString(host, username))
+  } else if (selection === connectOption) {
+    await connectToHost(host, username, path)
+  } else {
+    throw Error('Nothing was selected')
+  }
+}
 
 export async function activate (context: vscode.ExtensionContext) {
   const config = readConfig()
   const username = 'root'
-  const folder = '/home'
+  const path = '/home'
 
-  context.subscriptions.push(vscode.commands.registerCommand('remote-vm.connectToVm', async () => {
-    const client = createApiClient({
-      token: config.token
-    })
+  const client = createApiClient({
+    token: config.token
+  })
 
-    let host = ''
-
+  context.subscriptions.push(vscode.commands.registerCommand('remote-vm.createVM', async () => {
     try {
-      const dropletsResponse = await client.droplet.listDroplets({})
-      const droplets = dropletsResponse.data.droplets
-      if (droplets.length === 0) {
-        const createdDroplet = await createDroplet(client)
+      const createdDroplet = await createDroplet(client)
 
-        let fetchedDroplet: IDroplet | undefined
-        while (fetchedDroplet === undefined || hasPublicIP(fetchedDroplet)) {
-          const dropletResponse = await client.droplet.getDroplet({
-            droplet_id: createdDroplet.id
-          })
-          fetchedDroplet = dropletResponse.data.droplet
-        }
-
-        host = getDropletIP(fetchedDroplet)
-      } else {
-        const droplet = await selectDroplet(droplets)
-        host = getDropletIP(droplet)
+      let fetchedDroplet: IDroplet | undefined
+      while (fetchedDroplet === undefined || hasPublicIP(fetchedDroplet)) {
+        const dropletResponse = await client.droplet.getDroplet({ droplet_id: createdDroplet.id })
+        fetchedDroplet = dropletResponse.data.droplet
       }
 
-      const copyOption = 'Copy SSH command'
-      const connectOption = 'Connect'
-
-      const selection = await vscode.window.showInformationMessage('Remote VM is ready', copyOption, connectOption)
-
-      if (selection === copyOption) {
-        await vscode.env.clipboard.writeText(connectionString(host, username))
-      } else if (selection === connectOption) {
-        await connect(host, username, folder)
-      } else {
-        throw Error('Nothing was selected')
-      }
+      await copyOrConnect(fetchedDroplet, username, path)
     } catch (error) {
       vscode.window.showErrorMessage(`Error occurred: ${error}`)
     }
   }))
 
-  // context.subscriptions.push(vscode.commands.registerCommand('remote-vm.disconnectFromVm', async () => {
+  context.subscriptions.push(vscode.commands.registerCommand('remote-vm.connectToVm', async () => {
+    try {
+      const dropletsResponse = await client.droplet.listDroplets({})
+      const droplets = dropletsResponse.data.droplets
+      const droplet = await selectDroplet(droplets)
 
-  // })
+      await copyOrConnect(droplet, username, path)
+    } catch (error) {
+      vscode.window.showErrorMessage(`Error occurred: ${error}`)
+    }
+  }))
+
+  context.subscriptions.push(vscode.commands.registerCommand('remote-vm.destroyVM', async () => {
+    try {
+      const dropletsResponse = await client.droplet.listDroplets({})
+      const droplets = dropletsResponse.data.droplets
+      const droplet = await selectDroplet(droplets)
+
+      await client.droplet.deleteDroplet({ droplet_id: droplet.id })
+    } catch (error) {
+      vscode.window.showErrorMessage(`Error occurred: ${error}`)
+    }
+  }))
 }
 
 export function deactivate () {}
