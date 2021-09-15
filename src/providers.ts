2a52b1f75363ea6c069ca489ea911b9da6482d2d
Ivan Korotkov <korotkov.ivan.s@gmail.com>
2021-09-15 15:46:22 +0500

    feat: turn off rename/delete commands on vm parameters

--- src/providers.ts
+++ src/providers.ts
@@ -1,11 +1,12 @@
 import * as vscode from 'vscode'
 import { Vm } from './models'
 
-class VmTreeItem extends vscode.TreeItem {
-  constructor (props: { id?: string, label: string, description?: string, collapsibleState: vscode.TreeItemCollapsibleState }) {
+export class VmTreeItem extends vscode.TreeItem {
+  constructor (props: { id?: string, label: string, contextValue: string, description?: string, collapsibleState: vscode.TreeItemCollapsibleState }) {
     super(props.label, props.collapsibleState)
 
     this.id = props.id
+    this.contextValue = props.contextValue
     this.description = props.description
   }
 }
@@ -29,23 +30,55 @@ export class VmTreeDataProvider implements vscode.TreeDataProvider<VmTreeItem> {
       }
 
       const elements = [
-        new VmTreeItem({ label: `Region: ${item.region}`, collapsibleState: vscode.TreeItemCollapsibleState.None }),
-        new VmTreeItem({ label: `OS: ${item.os}`, collapsibleState: vscode.TreeItemCollapsibleState.None }),
-        new VmTreeItem({ label: `CPUs: ${item.cpus}`, collapsibleState: vscode.TreeItemCollapsibleState.None }),
-        new VmTreeItem({ label: `RAM: ${item.ram} GB`, collapsibleState: vscode.TreeItemCollapsibleState.None }),
-        new VmTreeItem({ label: `Storage: ${item.storage} GB`, collapsibleState: vscode.TreeItemCollapsibleState.None })
+        new VmTreeItem({
+          label: `Region: ${item.region}`,
+          contextValue: 'parameter',
+          collapsibleState: vscode.TreeItemCollapsibleState.None
+        }),
+        new VmTreeItem({
+          label: `OS: ${item.os}`,
+          contextValue: 'parameter',
+          collapsibleState: vscode.TreeItemCollapsibleState.None
+        }),
+        new VmTreeItem({
+          label: `CPUs: ${item.cpus}`,
+          contextValue: 'parameter',
+          collapsibleState: vscode.TreeItemCollapsibleState.None
+        }),
+        new VmTreeItem({
+          label: `RAM: ${item.ram} GB`,
+          contextValue: 'parameter',
+          collapsibleState: vscode.TreeItemCollapsibleState.None
+        }),
+        new VmTreeItem({
+          label: `Storage: ${item.storage} GB`,
+          contextValue: 'parameter',
+          collapsibleState: vscode.TreeItemCollapsibleState.None
+        })
       ]
 
       if (item.ipv4 !== undefined) {
-        elements.push(new VmTreeItem({ label: `IPv4: ${item.ipv4 ?? '-'}`, collapsibleState: vscode.TreeItemCollapsibleState.None }))
+        elements.push(new VmTreeItem({
+          label: `IPv4: ${item.ipv4 ?? '-'}`,
+          contextValue: 'parameter',
+          collapsibleState: vscode.TreeItemCollapsibleState.None
+        }))
       }
 
       if (item.ipv6 !== undefined) {
-        elements.push(new VmTreeItem({ label: `IPv6: ${item.ipv6 ?? '-'}`, collapsibleState: vscode.TreeItemCollapsibleState.None }))
+        elements.push(new VmTreeItem({
+          label: `IPv6: ${item.ipv6 ?? '-'}`,
+          contextValue: 'parameter',
+          collapsibleState: vscode.TreeItemCollapsibleState.None
+        }))
       }
 
       if (item.tags.length > 0) {
-        elements.push(new VmTreeItem({ label: `Tags: ${item.tags.join(', ')}`, collapsibleState: vscode.TreeItemCollapsibleState.None }))
+        elements.push(new VmTreeItem({
+          label: `Tags: ${item.tags.join(', ')}`,
+          contextValue: 'parameter',
+          collapsibleState: vscode.TreeItemCollapsibleState.None
+        }))
       }
 
       return Promise.resolve(elements)
@@ -59,6 +92,7 @@ export class VmTreeDataProvider implements vscode.TreeDataProvider<VmTreeItem> {
         const element = new VmTreeItem({
           id: item.id.toString(),
           label: item.name,
+          contextValue: 'vm',
           description: item.status,
           collapsibleState: vscode.TreeItemCollapsibleState.Collapsed
         })
